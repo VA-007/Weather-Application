8a00f1f8802d906e38f57c182efa2539fa8e8109
VA-007 <vishanksharma20102000@gmail.com>
2020-09-09 21:57:24 +0530

    Added UV index feature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,20 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Please provide a valid set of coordinates', undefined)
     } else {
-      callback(undefined, `${body.current.weather_descriptions}. The current temperature is ${body.current.temperature}°C, visibility is ${body.current.visibility}, with a humidity of ${body.current.humidity}.`);
+      const uvIndex = body.current.uv_index;
+
+      // Setting up forecast according to the uv index value.
+      if (uvIndex <= 2) {
+        callback(undefined, `${body.current.weather_descriptions}. The current temperature is ${body.current.temperature}°C, visibility is ${body.current.visibility}, with a humidity of ${body.current.humidity}. The UV index in your area is ${uvIndex}. A UV index reading of 0 to 2 means LOW DANGER from the Sun's UV rays for the average person.`);
+      } else if (3 <= uvIndex <= 5) {
+        callback(undefined, `${body.current.weather_descriptions}. The current temperature is ${body.current.temperature}°C, visibility is ${body.current.visibility}, with a humidity of ${body.current.humidity}. The UV index in your area is ${uvIndex}. A UV index reading of 3 to 5 means MODERATE RISK of harm from unprotected sun exposure.`);
+      } else if (6 <= uvIndex <= 7) {
+        callback(undefined, `${body.current.weather_descriptions}. The current temperature is ${body.current.temperature}°C, visibility is ${body.current.visibility}, with a humidity of ${body.current.humidity}. The UV index in your area is ${uvIndex}. A UV index reading of 6 to 7 means HIGH RISK of harm from unprotected sun exposure. Protection against skin and eye damage is needed.`);
+      } else if (8 <= uvIndex <= 10) {
+        callback(undefined, `${body.current.weather_descriptions}. The current temperature is ${body.current.temperature}°C, visibility is ${body.current.visibility}, with a humidity of ${body.current.humidity}. The UV index in your area is ${uvIndex}. A UV index reading of 8 to 10 means VERY HIGH RISK of harm from unprotected sun exposure. Take extra precautions because unprotected skin and eyes will be damaged and can burn quickly.`);
+      } else {
+        callback(undefined, `${body.current.weather_descriptions}. The current temperature is ${body.current.temperature}°C, visibility is ${body.current.visibility}, with a humidity of ${body.current.humidity}. The UV index in your area is ${uvIndex}. A UV index reading of 11 or more means EXTREME RISK of harm from unprotected sun exposure. Take all precautions because unprotected skin and eyes can burn in minutes.`);
+      }
     }
   });
 }
